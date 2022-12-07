33340a030c5cf012537aa272d6995e000b3afb0b
Lauren RAKOTOBE <lauren.rakotobe@efrei.net>
2022-12-07 12:21:12 +0100

    add postgres config

--- src/lib/typeorm.ts
+++ src/lib/typeorm.ts
@@ -8,9 +8,9 @@ import { ValidationSubscriber } from '../subscribers/ValidationSubscriber'
 dotenv.config()
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
+    type: "postgres",
+    host: "127.0.0.1",
+    port: 5432,
     username: process.env.DBB_USERNAME,
     password: process.env.DBB_PASSWORD,
     database: process.env.NODE_ENV === 'test' ? "iam_test": 'iam',
